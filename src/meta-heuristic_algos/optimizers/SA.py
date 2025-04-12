e34ac17fcd03e31e8e1a038cdfd890815ad59d3f
alvin0603 <alvinhuang0603@gmail.com>
2025-04-13 02:29:13 +0800

    update SA

--- src/meta-heuristic_algos/optimizers/SA.py
+++ src/meta-heuristic_algos/optimizers/SA.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from DataSet import DataSet
 
 class SA:
-    def __init__(self, obj_function, dim, lb, ub, max_iter, f_type, init_temp=1000, cooling_rate=0.95):
+    def __init__(self, obj_function, dim, lb, ub, max_iter, f_type, init_temp=1000, cooling_rate=0.95, init_population=None):
         self.obj_function = obj_function
         self.dim = dim
         self.lb = np.array(lb)
@@ -19,8 +19,18 @@ class SA:
             self.lb = np.append(self.lb, 1)
             self.dim += 1
 
-        # initialization
-        self.current = np.random.uniform(self.lb, self.ub, self.dim)
+        if init_population is None:
+            self.current = np.random.uniform(self.lb, self.ub, self.dim)
+            self.population = None
+            self.candidate_index = None
+        else:
+            pop = np.array(init_population)
+            fitnesses = [self.obj_function(ind) for ind in pop]
+            best_idx = np.argmin(fitnesses)
+            self.current = pop[best_idx].copy()
+            self.candidate_index = best_idx
+            self.population = pop
+        
         self.current_fitness = self.obj_function(self.current)
         self.best = self.current.copy()
         self.best_fitness = self.current_fitness
@@ -61,7 +71,12 @@ class SA:
 
             convergence_curve.append(self.best_fitness)
             self.temp *= self.cooling_rate
-        return self.best, self.best_fitness, convergence_curve, self.current
+    
+        if self.population is not None and self.candidate_index is not None: # return updated population for high-level contor
+            self.population[self.candidate_index] = self.best.copy()
+            return self.best, self.best_fitness, convergence_curve, self.population
+        else:
+            return self.best, self.best_fitness, convergence_curve, self.current
 
 class SACONTROL:
     __name__ = "SA"
@@ -75,16 +90,16 @@ class SACONTROL:
         self.f = FUNCTION.func
         self.f_type = FUNCTION.f_type
 
-    def Start(self):
+    def Start(self, init_population=None):
         sa = SA(obj_function=self.f, dim=self.DIM, lb=self.LB, ub=self.UB,
                 max_iter=self.MAX_ITER, init_temp=self.INIT_TEMP, cooling_rate=self.COOLING_RATE,
-                f_type=self.f_type)
-        best_position, best_value, curve, final_solution = sa.optimize()
+                f_type=self.f_type, init_population=init_population)
+        best_position, best_value, curve, updated_population = sa.optimize()
 
         if self.f_type == "d":
-            return (final_solution, np.array(curve))
+            return (updated_population, np.array(curve))
         else:
-            return (final_solution, np.log10(curve))
+            return (updated_population, np.log10(curve))
 
 if __name__ == '__main__':
     funcs_by_year = DataSet.funcs_years
@@ -104,7 +119,7 @@ if __name__ == '__main__':
 
             sa = SA(obj_function=f, dim=DIM, lb=LB, ub=UB, max_iter=MAX_ITER,
                     init_temp=INIT_TEMP, cooling_rate=COOLING_RATE, f_type=function.f_type)
-            best_position, best_value, curve, final_solution = sa.optimize()
+            best_position, best_value, curve, updated_population = sa.optimize()
 
             print(f"[CEC {year}-{func_name}] Best solution found:", best_position)
             print(f"[CEC {year}-{func_name}] Best fitness:", best_value)
